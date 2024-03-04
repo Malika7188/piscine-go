d14a3f649a007a2c1370674c271625d3a21b6988
masman <asmanodeny@gmail.com>
2024-03-04 15:16:02 +0300

    iterativefactorial.go

---
+++ iterativefactorial.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	if nb < 0 || nb > 20 {
+		return 0
+	} else if nb == 0 {
+		return 1
+	} else {
+		k := nb - 1
+		for i := k; i >= 1; i-- {
+			nb *= i
+		}
+	}
+	return nb
+
+}
