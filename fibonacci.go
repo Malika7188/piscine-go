e51801d436dab05b2558558dbda57e745478c834
masman <asmanodeny@gmail.com>
2024-03-05 19:04:16 +0300

    Fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
 package piscine
-=======
-package main
->>>>>>> 302935588efd1f5ce8b0495b1eeb468e2bbea5d6
 
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
 	} else if index == 1 {
-<<<<<<< HEAD
 		return index
 	} else {
 		return Fibonacci(index-2) + Fibonacci(index-1)
 	}
-=======
-		return 1 
-	} else if index == 0 {
-		return 0 
-	} else {
-		return Fibonacci(index-2) + Fibonacci(index-1)
-	}
-
->>>>>>> 302935588efd1f5ce8b0495b1eeb468e2bbea5d6
 }
