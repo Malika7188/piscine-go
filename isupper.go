95f6e546d7317aac191edaa1a64e45b47b8a8aea
masman <asmanodeny@gmail.com>
2024-03-07 18:43:47 +0300

    isupper

---
+++ isupper.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsUpper(s string) bool {
+	for _, r := range s {
+		if r <= 'A' && r <= 'Z' {
+			continue
+		} else if r >= 'a' && r <= 'Z' {
+			return false
+		} else {
+			return false
+		}
+	}
+	return true
+
+}
