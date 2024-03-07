170a3ba450a75a48fe1a9dabf53fda0a4e79420c
Christopher Fremond <dev@01-edu.org>
2024-03-07 16:58:00 +0000

    Update 'strlen.go'

--- strlen.go
+++ strlen.go
@@ -1,11 +1,11 @@
 package piscine
 
 func StrLen(s string) int {
-	nb := 0
-	for i := range []rune(s) {
-		if i == i {
-			nb++
-		}
-	}
-	return nb
+  nb := 0
+  for i := range []rune(s) {
+    if i == i{
+      nb++
+    }
+  }
+  return nb
 }
