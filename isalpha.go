ceb850a29bdfa70362f2ef4843ef1629bb6fcff2
masman <asmanodeny@gmail.com>
2024-03-07 21:22:31 +0300

    Is Alpha

--- isalpha.go
+++ isalpha.go
@@ -3,7 +3,7 @@ package piscine
 func IsAlpha(s string) bool {
 	for _, i := range s {
 		if (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') || (i >= '0' && i <= '9') {
-			return true
+			continue
 		} else {
 			return false
 		}
