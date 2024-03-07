58e4162b98534abfb24bfe93d86dd3b321fc9d17
Christopher Fremond <dev@01-edu.org>
2024-03-07 11:46:18 +0000

    Update 'compare.go'

--- compare.go
+++ compare.go
@@ -1,21 +1,15 @@
-package piscine
+package main
 
-import "github.com/01-edu/z01"
+func Compare(a, b string) int {
+  //sizea := o
+  //sizeb := o
+  if a == b {
+    return 0
+  } else if a > b {
+    return 1
+  } else {
+    retun -1
+  }
 
-func PrintComb() {
-	for a := '0'; a <= '7'; a++ {
-		for b = a + 1; b <= '8'; b++ {
-			for c := b + 1; c <= '9'; c++ {
-				z01.PrintRune(a)
-				z01.PrintRune(b)
-				z01.PrintRune(c)
-				if a == '7'&& b == '8'&& c == '9' {
-					z01.PrintRune('\n')
-				}else {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				} 				
-			}
-		}
-	}
 }
+
