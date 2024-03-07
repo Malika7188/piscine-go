14eeba492274d23c1ade220a616bf35fb8e62e5a
masman <asmanodeny@gmail.com>
2024-03-07 16:54:39 +0300

    print combinations

--- printcomb.go
+++ printcomb.go
@@ -3,19 +3,19 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := '0'; i <= '6'; i++ {
-		for j := i + 1; i <= '7'; j++ {
-			for k := j + 1; i <= '8'; k++ {
-				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(k)
-				if i == 6 || j == 7 || k == 8 {
-					return
+	for a := '0'; a <= '7'; a++ {
+		for b := a + 1; b <= '8'; b++ {
+			for c := b + 1; c <= '9'; c++ {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
+				if a == '7' && b == '8' && c == '9' {
+					z01.PrintRune('\n')
+				} else {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 				}
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
 			}
 		}
 	}
-
 }
