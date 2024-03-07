4d4ce3437d71266f5ab9cf496a7a265448910062
Christopher Fremond <dev@01-edu.org>
2024-03-07 09:58:52 +0000

    Update 'printcomb.go'

--- printcomb.go
+++ printcomb.go
@@ -3,19 +3,23 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := '0'; i <= '6'; i++ {
-		for j := i + 1; i <= '7'; j++ {
-			for k := j + 1; i <= '8'; k++ {
-				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(k)
-				if i == 6 || j == 7 || k == 8 {
-					return
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; i <= '8'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				if i < j {
+					if j < k {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(k)
+						if i == '7' && j == '8' && k == '9' {
+							z01.PrintRune('\n')
+						} else {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
 				}
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
 			}
 		}
 	}
-
 }
