8e776a39d0089ab4aac7a297676282586d9146ba
Malika Asman <masman@noreply.learn.zone01kisumu.ke>
2024-03-08 21:57:25 +0000

    Update 'sortparams/main.go'

--- sortparams/main.go
+++ sortparams/main.go
@@ -8,14 +8,14 @@ import (
 
 func main() {
 	argument := os.Args[1:]
-	for i := 0; i < len(argument) -1; i++ {
-		for j := 0; j < len(argument)-i-1; j++{
-			if argument[j] > argument[j+1]{
-				argument[j],argument[i] = argument[i], argument[j]
+	for i := 0; i < len(argument)-1; i++ {
+		for j := 0; j < len(argument)-i-1; j++ {
+			if argument[j] > argument[j+1] {
+				argument[j], argument[i] = argument[i], argument[j]
 			}
 		}
 	}
-	for _, str := range argument{
+	for _, str := range argument {
 		for _, cha := range str {
 			z01.PrintRune(cha)
 		}
