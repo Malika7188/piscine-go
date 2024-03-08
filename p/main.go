b417dc5a00976ee9afb7a55ae225bede5800956d
Malika Asman <masman@noreply.learn.zone01kisumu.ke>
2024-03-08 21:40:18 +0000

    Upload files to 'p'

---
+++ p/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	argument := os.Args[1:]
+	for _, val := range argument {
+		for _, char := range val {
+			z01.PrintRune(char)
+		}
+	}
+	z01.PrintRune('\n')
+}
