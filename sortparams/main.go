49ef17a90d7a3b9c6fe472a826261feab164a311
Malika Asman <masman@noreply.learn.zone01kisumu.ke>
2024-03-08 21:52:48 +0000

    Upload files to 'sortparams'

---
+++ sortparams/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	argument := os.Args[1:]
+	for i := 0 i < len(aargument) -1; i++ {
+		for j := 0; j < len(argument)-i-1; j++{
+			if argument[j] > argument[j+1]{
+				argument[j],argument[i] = argument[i], argument[j]
+			}
+		}
+	}
+	for _, str := range argument{
+		for _, cha := range str {
+			z01/PrintRune(cha)
+		}
+		z01.PrintRune('\n')
+	}
+}
