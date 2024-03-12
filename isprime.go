8da747ada41df6a4965b3c4db9ee578cc8153dcb
masman <asmanodeny@gmail.com>
2024-03-12 18:15:46 +0300

    modified

--- isprime.go
+++ isprime.go
@@ -4,10 +4,18 @@ func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
 	}
-	for i := 2; i <= nb/2; i++ {
-		if nb%2 == 0 {
+	if nb <= 3 {
+		return true
+	}
+	if nb%2 == 0 || nb%3 == 0 {
+		return false
+	}
+	k := 5
+	for k*k <= nb {
+		if nb%k == 0 || nb%(k+2) == 0 {
 			return false
 		}
+		k += 6
 	}
 	return true
 }
