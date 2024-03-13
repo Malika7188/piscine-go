696ff4e8f07e27d79b8f4c011b17075222556682
masman <asmanodeny@gmail.com>
2024-03-14 02:36:16 +0300

    reversemenue

---
+++ reversemenuindex.go
@@ -0,0 +1,10 @@
+package piscine
+
+func ReverseMenuIndex(menu []string) []string {
+	arg := make([]string, len(menu))
+
+	for i := len(menu) - 1; i >= 0; i-- {
+		arg[len(menu)-1-i] = menu[i]
+	}
+	return arg
+}
