6836d14dbdfe94bd8a03f140870951d75e198061
Christopher Fremond <dev@01-edu.org>
2024-03-13 08:00:01 +0000

    Update 'rot14.go'

--- rot14.go
+++ rot14.go
@@ -5,9 +5,9 @@ func Rot14(s string) string {
   
   for _, ch := range s {
     if ch >= 'A' && ch <= 'Z' {
-      k += k + string('A' + (ch-'A'+14)%26)
+      k = k + string('A' + (ch-'A'+14)%26)
     } else if ch >= 'a' && ch <= 'z' {
-      k += string('a' + (ch-'a'+14)%26)
+      k =k + string('a' + (ch-'a'+14)%26)
     } else {
       k += string(ch)
     }
