4e86ddeae2c21614a9f2062f53583fb459819efd
masman <asmanodeny@gmail.com>
2024-03-13 12:00:16 +0300

    desecend

--- rot14.go
+++ rot14.go
@@ -1,17 +1,16 @@
 package piscine
 
 func Rot14(s string) string {
-  var k string
-  
-  for _, ch := range s {
-    if ch >= 'A' && ch <= 'Z' {
-      k = k + string('A' + (ch-'A'+14)%26)
-    } else if ch >= 'a' && ch <= 'z' {
-      k =k + string('a' + (ch-'a'+14)%26)
-    } else {
-      k += string(ch)
-    }
-  }
-  return k
-}
+	var k string
 
+	for _, ch := range s {
+		if ch >= 'A' && ch <= 'Z' {
+			k = k + string('A'+(ch-'A'+14)%26)
+		} else if ch >= 'a' && ch <= 'z' {
+			k = k + string('a'+(ch-'a'+14)%26)
+		} else {
+			k += string(ch)
+		}
+	}
+	return k
+}
