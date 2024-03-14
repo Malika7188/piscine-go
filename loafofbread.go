5b3a3471a9498790c83f00332931f79aae742f85
masman <asmanodeny@gmail.com>
2024-03-14 09:15:06 +0300

    loaf

--- loafofbread.go
+++ loafofbread.go
@@ -7,7 +7,7 @@ func LoafOfBread(str string) string {
 	result := ""
 	for i := 0; i < len(str); i++ {
 		if i != 0 {
-			if (i+1)%5 == 0 {
+			if (i+1)%5 == 2 {
 				result += string(str[i])
 			}
 		}
