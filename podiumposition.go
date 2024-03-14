670ffe1c9011c4cfde77f769474b4c7b9264a5a0
masman <asmanodeny@gmail.com>
2024-03-14 03:35:20 +0300

    podium

---
+++ podiumposition.go
@@ -0,0 +1,11 @@
+package piscine
+
+func PodiumPosition(podium [][]string) [][]string {
+	r := make([][]string, len(podium))
+
+	for i := len(podium); i >= 0; i++ {
+		r[len(podium)-1-i] = podium[i]
+
+	}
+	return r
+}
