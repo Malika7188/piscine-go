0e3bd0fbdea417af3f7f228da11f0302308c0cc6
masman <asmanodeny@gmail.com>
2024-03-14 09:12:23 +0300

    loaf

---
+++ loafofbread.go
@@ -0,0 +1,17 @@
+package piscine
+
+func LoafOfBread(str string) string {
+	if str == "" || len(str) < 5 {
+		return "\n"
+	}
+	result := ""
+	for i := 0; i < len(str); i++ {
+		if i != 0 {
+			if (i+1)%5 == 0 {
+				result += string(str[i])
+			}
+		}
+	}
+	result += "\n"
+	return result
+}
