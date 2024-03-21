26f09475c1ae323da562cbc9f5fdd1ff2038a58f
masman <asmanodeny@gmail.com>
2024-03-21 11:25:46 +0300

    btreeaply

---
+++ btreeapplyinorder.go
@@ -0,0 +1,10 @@
+package piscine
+
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+	f(root.Data)
+	BTreeApplyInorder(root.Left, f)
+	BTreeApplyInorder(root.Right, f)
+}
