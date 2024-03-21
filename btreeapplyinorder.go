811ca041b4fa1067dbd7581f804fb2e57157d83c
masman <asmanodeny@gmail.com>
2024-03-21 11:29:50 +0300

    btreeaply

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -4,7 +4,7 @@ func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	f(root.Data)
-	BTreeApplyInorder(root.Left, f)
 	BTreeApplyInorder(root.Right, f)
+	BTreeApplyInorder(root.Left, f)
+	f(root.Data)
 }
